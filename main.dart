b14ee95be113a31fd4847aa23938201c51ea1736
Sipaami <aykookie12@gmail.com>
2022-04-22 13:32:29 +0800

    output2t

--- main.dart
+++ main.dart
@@ -1,37 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp (MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  final List<Color>myColor=[
-    Colors.pink,
-    Colors.blue,
-    Colors.purple,
-    Colors.amber
-  ];
-  final List<Widget>myList =List.generate(
-    100,
-    (index) => Text(
-      "${index+1}",
-      style: TextStyle(
-        fontSize: 15+
-        double.parse(index.toString()),
-      ),
-    ),
-  );
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
- @override
-Widget build (BuildContext context) {
-  return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text("S i p a a m i"),
-       ),
-        body: ListView(
-       children: myList,
-      )
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      );
-}
+      home: Scaffold(
+        body: Center(child: Text("Cipaa")),
+        appBar: AppBar(
+          title: Text("sipaami"),
+        )
+      ),
+    );
+  }
 }
